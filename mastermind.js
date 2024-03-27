967cbda5aae90641c83100bf0937243fdae91307
ronanmdaly <156950186+ronanmdaly@users.noreply.github.com>
2024-03-27 14:21:29 -0400

    adding classes

--- mastermind.js
+++ mastermind.js
@@ -1,132 +1,154 @@
-const main_display = document.querySelector('main');
-const div_select_colors = document.getElementById('div-select-color');
-const crack_button = document.getElementById('crack-btn');
-
-let codeLength = 4;
-let tries = 8;
-let colors = ['blue', 'yellow', 'orange', 'green', 'red', 'purple'];
-
-let random_code = [];
-let crackTry = 1;
-
-
-init();
-
-function init() {
-    random_code = [];
-    crackTry = 1;
-    main_display.innerHTML = '';
-    div_select_colors.innerHTML = '';
-    for (let i = 1; i <= trys; i++) {
-        let div_try = document.createElement('div');
-        div_try.setAttribute('id', 'try-'+i);
-        div_try.setAttribute('class', 'try');
-        let div_left = document.createElement('div');
-        div_left.setAttribute('class', 'left');
-        let div_right = document.createElement('div');
-        div_right.setAttribute('class', 'right');
-
-        for (let i = 1; i <= codeLength; i++) {
-            let div_l = document.createElement('div');
-            let div_r = document.createElement('div');
-            div_left.append(div_l);
-            div_right.append(div_r);
-        }
-
-        div_try.append(div_left);
-        div_try.append(div_right);
-        main_display.prepend(div_try);
-        
+class Game {
+    constructor() {
+        this.main_display = document.querySelector('main');
+        this.div_select_colors = document.getElementById('div-select-color');
+        this.crack_button = document.getElementById('crack-btn');
+        this.length = 4;
+        this.trys = 8;
+        this.colors = ['blue', 'yellow', 'orange', 'green', 'red', 'purple'];
+        this.random_code = [];
+        this.checkTry = 1;
+        this.init();
     }
-    for (let i = 1; i <= codeLength; i++) {
-        let div_select_wrapper = document.createElement('div');
-        div_select_wrapper.setAttribute('class', 'select-wrapper');
-        let select = document.createElement('select');
 
-        for (let color of colors) {
-            let option = document.createElement('option');
-            option.setAttribute('style', 'background-color:'+color);
-            option.setAttribute('value', color);
-            select.append(option);
+    init() {
+        this.random_code = this.createRandomCode();
+        this.checkTry = 1;
+        this.main_display.innerHTML = '';
+        this.div_select_colors.innerHTML = '';
+        this.board = new Board(this.main_display, this.trys, this.length);
+        this.board.createTries();
+        this.createColorSelectors();
+        this.setupListeners();
+    }
+
+    createRandomCode() {
+        let random_code = [];
+        for (let i = 0; i < this.length; i++) {
+            let random_color = this.colors[Math.floor(Math.random() * this.colors.length)];
+            random_code.push(random_color);
         }
-        select.setAttribute('style', 'background-color:'+colors[0]);
+        console.log(random_code);
+        return random_code;
+    }
 
-        select.addEventListener('change', (e) => {
-            e.target.setAttribute('style', 'background-color:'+e.target.value);
+    createColorSelectors() {
+        for (let i = 0; i < this.length; i++) {
+            let div_select_wrapper = document.createElement('div');
+            div_select_wrapper.className = 'select-wrapper';
+            let select = document.createElement('select');
+
+            this.colors.forEach(color => {
+                let option = document.createElement('option');
+                option.value = color;
+                option.style.backgroundColor = color;
+                select.appendChild(option);
+            });
+
+            select.style.backgroundColor = select.value;
+            select.addEventListener('change', (e) => {
+                e.target.style.backgroundColor = e.target.value;
+            });
+
+            div_select_wrapper.appendChild(select);
+            this.div_select_colors.appendChild(div_select_wrapper);
+        }
+    }
+
+    setupListeners() {
+        this.crack_button.addEventListener('click', () => {
+            let input_colors = Array.from(this.div_select_colors.querySelectorAll('select')).map(select => select.value);
+            this.board.show('left', input_colors, this.checkTry);
+            let correctionArray = this.createCorrectionArray(input_colors);
+            this.board.show('right', correctionArray, this.checkTry);
+            this.checkTry++;
+            this.checkWin(correctionArray);
         });
-
-        div_select_wrapper.append(select);
-        div_select_colors.append(div_select_wrapper);
     }
-    createRandomCode();
-}
 
-function createRandomCode() {
-    for (let i = 1; i <= codeLength; i++) {
-        let random_color = colors[Math.floor(Math.random() * colors.length)]
-        random_code.push(random_color);
-    }
-    console.log(random_code);
-}
+    createCorrectionArray(input_colors_arr) {
+        let red_pegs = [];
+        let white_pegs = [];
+        let black_pegs = new Array(this.length).fill('black');
+        let random_code_copy = [...this.random_code];
 
-crack_button.addEventListener('click', (e) => {
-    
-    let input_colors = document.querySelectorAll('.select-wrapper>select');
-    let input_colors_arr = [];
-    for (let v of input_colors) {
-        input_colors_arr.push(v.value);
-    }
-    show('left', input_colors_arr);
-    correction_Array = createCorrectionArray(input_colors_arr);
-    show('right', correction_Array);
-    crackTry++;
-    checkWin(correction_Array);
-
-});
-
-
-function show(type, colors) {
-    let tryView = document.querySelectorAll('#try-'+crackTry+'>.'+type+'>div');
-    tryView.forEach((v, i) => {
-        v.setAttribute('style', 'background-color:'+colors[i]);
-    });
-}
-
-function createCorrectionArray(input_colors_arr) {
-    let random_code_copy = [...random_code];
-    let correction_Array = [];
-    
-    for (let i in random_code_copy) {
-        if (random_code_copy[i] == input_colors_arr[i])  {
-            random_code_copy[i] = null;
-            input_colors_arr[i] = null;
-            correction_Array.push('red');
-        }
-    }
-    for (let i in random_code_copy) {
-        for (j in input_colors_arr) {
-            if (random_code_copy[i] != null && random_code_copy[i] == input_colors_arr[j]) {
+        for (let i = 0; i < this.length; i++) {
+            if (input_colors_arr[i] === random_code_copy[i]) {
+                red_pegs.push('red');
+                input_colors_arr[i] = null;
                 random_code_copy[i] = null;
-                input_colors_arr[j] = null;
-                correction_Array.push('white');
             }
         }
-    }
-    return correction_Array;
-}
 
-function checkWin(correction_Array) {
-    let countCorrect = 0;
-    for (let v of correction_Array) {
-        if (v == 'red') {
-            countCorrect++;
+        for (let i = 0; i < this.length; i++) {
+            if (input_colors_arr[i] !== null) {
+                const foundIndex = random_code_copy.findIndex(color => color === input_colors_arr[i]);
+                if (foundIndex !== -1) {
+                    white_pegs.push('white');
+                    random_code_copy[foundIndex] = null;
+                }
+            }
+        }
+
+        let correction_Array = red_pegs.concat(white_pegs).concat(black_pegs.slice(0, this.length - red_pegs.length - white_pegs.length));
+        return correction_Array;
+    }
+
+    checkWin(correctionArray) {
+        let correctCount = correctionArray.filter(color => color === 'red').length;
+        if (correctCount === this.length) {
+            alert('You won!');
+            this.init();
+        } else if (this.checkTry > this.trys) {
+            alert('You lose!');
+            this.init();
         }
     }
-    if (countCorrect == codeLength) {
-        alert('You won!');
-        init();
-    } else if(crackTry > trys) {
-        alert('You lose!');
-        init();
+}
+
+class Board {
+    constructor(main_display, trys, length) {
+        this.main_display = main_display;
+        this.trys = trys;
+        this.length = length;
+    }
+
+    createTries() {
+        for (let i = 1; i <= this.trys; i++) {
+            let div_try = document.createElement('div');
+            div_try.id = 'try-' + i;
+            div_try.className = 'try';
+            let div_left = document.createElement('div');
+            div_left.className = 'left';
+            let div_right = document.createElement('div');
+            div_right.className = 'right';
+
+            for (let j = 0; j < this.length; j++) {
+                div_left.appendChild(document.createElement('div'));
+                div_right.appendChild(document.createElement('div'));
+            }
+
+            div_try.appendChild(div_left);
+            div_try.appendChild(div_right);
+            this.main_display.appendChild(div_try);
+        }
+    }
+
+    show(side, colors, checkTry) {
+        const tryDiv = this.main_display.querySelector(`#try-${checkTry} .${side}`);
+        const divs = tryDiv.querySelectorAll('div');
+
+        // Clear previous colors
+        divs.forEach(div => div.style.backgroundColor = '');
+
+        // Set new colors
+        colors.forEach((color, index) => {
+            if (divs[index]) {
+                divs[index].style.backgroundColor = color;
+            }
+        });
     }
 }
+
+// Instantiate and start the game
+const game = new Game();
